b956154179b8298b89116f73f0f4d70d34e606e5
Anton Tolstonogov <anton_tolstonogov@cashalot.co>
2023-06-02 17:30:10 +0900

    refactor(drivers): Сделать тестовый launch более читабельным
    Сделан launch файл более читабельным, добавлено групповое пространство
    имён

--- reference_square/launch/reference_square.launch.py
+++ reference_square/launch/reference_square.launch.py
@@ -1,23 +1,40 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
-
+from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import GroupAction
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='reference_square',
-            executable='image_pub',
-        ),
-        Node(
-            package='reference_square',
-            executable='odom_pub',
-        ),
-        Node(
-            package='reference_square',
-            executable='camerainfo_pub',
-        ),
-        Node(
-            package='reference_square',
-            executable='reference_square',
-        ),
+    namespace = 'solaster'
+
+    reference_square_node = Node(package='reference_square',
+                                 executable='image_pub',
+                                 output='screen',
+                                 emulate_tty=True
+                                 )
+    
+    test_odom_pub = Node(package='reference_square',
+                         executable='odom_pub',
+                         output='screen',
+                         emulate_tty=True
+                         )
+
+    test_camera_info_pub = Node(package='reference_square',
+                                executable='camerainfo_pub',
+                                output='screen',
+                                emulate_tty=True
+                                )
+    
+    test_image_pub = Node(package='reference_square',
+                          executable='reference_square',
+                          output='screen',
+                          emulate_tty=True
+                          )
+
+    group = GroupAction([
+        PushRosNamespace(namespace),
+        reference_square_node,
+        test_odom_pub,
+        test_camera_info_pub,
+        test_image_pub
     ])
+
+    return LaunchDescription([group])
